534c9125567c2bbcebfb243f11761e2d45a71df4
Oleg Burov <oleg.burov@outlook.com>
2016-10-21 19:24:22 -0700

    Implemented patch.

---
+++ src/Sitecore.Support.130247/ExperienceAnalytics/Core/Config.cs
@@ -0,0 +1,37 @@
+﻿using Sitecore.Configuration;
+using System;
+
+namespace Sitecore.Support.ExperienceAnalytics.Core
+{
+  /// <summary>
+  /// Copied-pasted from the origin class due to the 'internal' access modifier. 
+  /// </summary>
+  public static class Config
+  {
+    private static TimeSpan? cacheExpiration;
+
+    public const string ExperienceAnalyticsSection = "experienceAnalytics";
+
+    internal static TimeSpan InternalCacheExpirationDefault { get; private set; }
+
+    static Config()
+    {
+      InternalCacheExpirationDefault = TimeSpan.FromMinutes(5);
+    }
+
+    internal static TimeSpan InternalCacheExpiration
+    {
+      get
+      {
+        if (!cacheExpiration.HasValue)
+        {
+          cacheExpiration = Settings.GetTimeSpanSetting("ExperienceAnalytics.InternalCacheExpiration", InternalCacheExpirationDefault);
+        }
+
+        return cacheExpiration.Value;
+      }
+
+      set { cacheExpiration = value; }
+    }
+  }
+}
---
+++ src/Sitecore.Support.130247/ExperienceAnalytics/Core/Repositories/LocalDefinitionServiceBase.cs
@@ -0,0 +1,61 @@
+﻿using Sitecore.Data.DataProviders.Sql;
+using Sitecore.Data.SqlServer;
+using Sitecore.Diagnostics;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+
+namespace Sitecore.Support.ExperienceAnalytics.Core.Repositories
+{
+  /// <summary>
+  /// Copied-pasted from the origin class due to the 'internal' access modifier. 
+  /// </summary>
+  abstract class LocalDefinitionServiceBase<T>
+   where T : class
+  {
+    protected SqlServerDataApi SqlServerApi { get; set; }
+    protected abstract string SqlExpression { get; }
+
+    protected LocalDefinitionServiceBase([NotNull] string connectionStringName)
+    {
+      Assert.ArgumentNotNullOrEmpty(connectionStringName, "connectionStringName");
+
+      ConnectionStringSettings connectionString = ConfigurationManager.ConnectionStrings[connectionStringName];
+
+      if (connectionString != null && !string.IsNullOrEmpty(connectionString.ConnectionString))
+      {
+        this.SqlServerApi = new SqlServerDataApi(connectionString.ConnectionString);
+      }
+    }
+
+    protected LocalDefinitionServiceBase()
+    {
+    }
+
+    public List<T> GetEntities()
+    {
+      using (DataProviderReader reader = this.SqlServerApi.CreateReader(this.SqlExpression, new object[0]))
+      {
+        return this.GetEntities(reader.InnerReader);
+      }
+    }
+
+    public List<T> GetEntities(IDataReader reader)
+    {
+      var entities = new List<T>();
+
+      while (reader.Read())
+      {
+        T entity = this.CreateEntityFromReader(reader);
+        if (entity != null)
+        {
+          entities.Add(entity);
+        }
+      }
+
+      return entities;
+    }
+
+    protected abstract T CreateEntityFromReader(IDataRecord reader);
+  }
+}
